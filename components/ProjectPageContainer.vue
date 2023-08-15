31f2cd9f4c8d111ab2f9071b913d23dcfbdf5d58
saikksub <saikksub@gmail.com>
2023-08-15 16:11:37 +0530

    Adds page tempate

---
+++ components/ProjectPageContainer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="relative h-full">
+    <div
+      class="absolute top-0 right-0 left-0 h-12 flex items-center gap-3 px-3 border-b border-b-zinc-800"
+    >
+      <slot name="header" />
+    </div>
+    <div class="absolute top-12 right-0 bottom-0 left-0 p-3 overflow-y-auto">
+      <slot name="body" />
+    </div>
+  </div>
+</template>
--- pages/project/[id]/index.vue
+++ pages/project/[id]/index.vue
@@ -1,9 +1,22 @@
 <template>
   <NuxtLayout name="app">
-    <UContainer> here - {{ projectId }} </UContainer>
+    <ProjectPageContainer>
+      <template v-slot:header>
+        {{ project.name }}
+      </template>
+      <template v-slot:body> hello </template>
+    </ProjectPageContainer>
   </NuxtLayout>
 </template>
 
 <script setup>
+import { useProject } from "@/store/project";
+import ProjectPageContainer from "@/components/ProjectPageContainer.vue";
+
+const storeProject = useProject();
 const projectId = useRoute().params?.id || "";
+
+const project = computed(() => {
+  return storeProject.getProjects.find((x) => x._id === projectId) || {};
+});
 </script>
